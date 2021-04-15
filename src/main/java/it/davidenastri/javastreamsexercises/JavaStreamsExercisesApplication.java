d2436f80ab04dc4cceb9e8cd9cad95b0685570e3
Davide Nastri <d.nastri@gmail.com>
2021-04-15 20:44:49 +0200

    feat: Added Spring

---
+++ src/main/java/it/davidenastri/javastreamsexercises/JavaStreamsExercisesApplication.java
@@ -0,0 +1,13 @@
+package it.davidenastri.javastreamsexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStreamsExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStreamsExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/davidenastri/javastreamsexercises/JavaStreamsExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package it.davidenastri.javastreamsexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStreamsExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
